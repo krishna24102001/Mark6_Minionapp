6cba447d17f0554bee27f4b3a333a90e33c117b7
krishna24102001 <krishnargite1@gmail.com>
2022-10-29 00:50:49 +0530

    mark6 complete

--- app.js
+++ app.js
@@ -5,12 +5,32 @@
 
 var translatebtn = document.querySelector("#translator");
 var userInput = document.querySelector("#usertext");
+var output = document.querySelector("#output");
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
+
+function translatorurl(text){
+    return serverurl + "?" + "text=" + text
+};
+function errorHandler(error){
+    console.log("Error!", error);
+    alert("Try again later")
+}
+
+function clickHandler(){
+    // output.innerText="bananannananana "+ userInput.value;
+    var inputtext = userInput.value;
+    fetch(translatorurl(inputtext))
+        .then(response => response.json())
+        .then(json => {
+            var translatedtext = json.contents.translated;
+            output.innerText= translatedtext;
+        })
+    
+    .catch(errorHandler)
+};
+
 
 translatebtn.addEventListener("click", clickHandler)
 
-function clickHandler(){
-    console.log("Clicked");
-    console.log("Input: ", userInput.value);
-};
 
 
