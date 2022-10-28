bc4eef66d370ac6bbbf0f2755a3bf8c95573cbb6
krishna24102001 <krishnargite1@gmail.com>
2022-10-28 13:10:01 +0530

    started js

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("Hii ")
+// var username=prompt("Type username")
+// var message="Its working " + username;
+// alert(message)
+
+
+var translatebtn = document.querySelector("#translator");
+
+translatebtn.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!");
+})
